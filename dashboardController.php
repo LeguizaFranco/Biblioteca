c83a74ad97c52e60ecb7f64d1da136f6d34a8d05
LeguizaFranco <Francoleguiza002@gmail.com>
2024-10-20 22:56:36 -0300

    modificacion

--- dashboardController.php
+++
@@ -1,6 +0,0 @@
-class DashboardController {
-    public function show() {
-        // Aquí puedes cargar cualquier dato necesario
-        include 'views/dashboard.phtml'; // Asegúrate de que la ruta sea correcta
-    }
-}
